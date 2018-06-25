92974638b6ecb6d730fb52f533a1b06616264a4e
feng qijun <jun1st.feng@gmail.com>
2018-06-25 22:56:01 +0800

    install multiple npm packages

--- baidu/access_token.js
+++ baidu/access_token.js
@@ -1,5 +1,5 @@
-var https = require('https')
 var qs = require('querystring')
+var rp = require('request-promise');
 
 const params = qs.stringify({
   'grant_type': 'client_credentials',
@@ -7,12 +7,22 @@ const params = qs.stringify({
   'client_secret': 'WOUP4LITVW1zEbSI2VxkOGyQ7IiyGGlx'
 })
 
-https.get(
-  {
-    hostname: 'aip.baidubce.com',
-    path: '/oauth/2.0/token?' + params,
-    agent: false
-  }, function(res) {
-    res.pipe(process.stdout)
-  }
-)
+var options = {
+  uri: 'https://aip.baidubce.com/oauth/2.0/token',
+  qs: {
+    'grant_type': 'client_credentials',
+    'client_id': 'BFyWzh9xVbyerdHEy10qlSY9',
+    'client_secret': 'WOUP4LITVW1zEbSI2VxkOGyQ7IiyGGlx'
+  },
+  json: true
+}
+
+async function refreshAccessToken() {
+  let result = await rp(options)
+  console.log(result)
+  return result
+}
+
+let res = refreshAccessToken()
+console.log(res)
+
