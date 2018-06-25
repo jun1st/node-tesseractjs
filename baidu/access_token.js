b2655a159681b941d38b9871343236043c44cd47
feng qijun <jun1st.feng@gmail.com>
2018-06-25 23:31:18 +0800

    save to cache

--- baidu/access_token.js
+++ baidu/access_token.js
@@ -1,6 +1,10 @@
 var qs = require('querystring')
 var rp = require('request-promise');
 
+const Expirable = require('expirable')
+
+var cache = new Expirable('25 days')
+
 const params = qs.stringify({
   'grant_type': 'client_credentials',
   'client_id': 'BFyWzh9xVbyerdHEy10qlSY9',
@@ -17,12 +21,28 @@ var options = {
   json: true
 }
 
-async function refreshAccessToken() {
-  let result = await rp(options)
-  console.log(result)
-  return result
+function refreshAccessToken(cb) {
+  rp(options)
+    .then(data => {
+      cache.set('accessToken', data.access_token)
+      console.log('get from cache')
+      console.log(cache.get('accessToken', true))
+      return cb(data.access_token)
+    })
 }
 
-let res = refreshAccessToken()
-console.log(res)
+function getAccessToken(callback) {
+  let token = cache.get('accessToken', true)
+  if (token == undefined || token == null) {
+    refreshAccessToken(callback)
+  } else {
+    console.log('get token from cache')
+    callback(token)
+  }
+}
+
+getAccessToken(function (data) {
+  console.log(data)
+})
+
 
